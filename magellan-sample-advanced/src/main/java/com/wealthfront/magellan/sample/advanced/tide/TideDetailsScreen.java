e30e019aea5efcc739a9783920a004bf8a3d51cf
William Clausen <william.h.f.clausen@gmail.com>
2017-04-08 15:02:52 -0700

    Improve UI

--- magellan-sample-advanced/src/main/java/com/wealthfront/magellan/sample/advanced/tide/TideDetailsScreen.java
+++ magellan-sample-advanced/src/main/java/com/wealthfront/magellan/sample/advanced/tide/TideDetailsScreen.java
@@ -8,6 +8,8 @@ import com.wealthfront.magellan.sample.advanced.model.Observation;
 import com.wealthfront.magellan.sample.advanced.model.TideInfo;
 
 import java.math.BigDecimal;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -20,6 +22,13 @@ import static com.wealthfront.magellan.sample.advanced.SampleApplication.app;
 
 public class TideDetailsScreen extends RxScreen<TideDetailsView> {
 
+  public static final Comparator<Observation>
+      OBSERVATION_COMPARATOR = new Comparator<Observation>() {
+    @Override
+    public int compare(Observation o1, Observation o2) {
+      return o1.getVerifiedWaterLevel().compareTo(o2.getVerifiedWaterLevel());
+    }
+  };
   @Inject NoaaApi noaaApi;
   private final String tideLocationName;
   int noaaApiId;
@@ -50,9 +59,14 @@ public class TideDetailsScreen extends RxScreen<TideDetailsView> {
           public void call(TideInfo tideInfo) {
             if (!tideInfo.getData().isEmpty()) {
               List<Observation> observations = tideInfo.getData();
+              BigDecimal highestMeasuredTideHeight =
+                  Collections.max(observations, OBSERVATION_COMPARATOR).getVerifiedWaterLevel();
+              BigDecimal lowestMeasuredTideHeight =
+                  Collections.min(observations, OBSERVATION_COMPARATOR).getVerifiedWaterLevel();
               BigDecimal latestMeasuredTideHeight =
                   observations.get(observations.size() - 1).getVerifiedWaterLevel();
-              getView().setLatestMeasuredTideHeight(latestMeasuredTideHeight);
+              getView().setTideHeights(latestMeasuredTideHeight, lowestMeasuredTideHeight,
+                  highestMeasuredTideHeight);
             }
           }
         }));
--- magellan-sample-advanced/src/main/java/com/wealthfront/magellan/sample/advanced/tide/TideDetailsView.java
+++ magellan-sample-advanced/src/main/java/com/wealthfront/magellan/sample/advanced/tide/TideDetailsView.java
@@ -4,6 +4,8 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -11,6 +13,7 @@ import com.wealthfront.magellan.BaseScreenView;
 import com.wealthfront.magellan.sample.advanced.R;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Locale;
 
 import butterknife.BindView;
@@ -22,6 +25,10 @@ class TideDetailsView extends BaseScreenView<TideDetailsScreen> {
   @BindView(R.id.loading) ProgressBar loading;
   @BindView(R.id.tideDetailsContent) View content;
   @BindView(R.id.currentWaterLevel) TextView currentWaterLevel;
+  @BindView(R.id.highestWaterLevel) TextView highestWaterLevel;
+  @BindView(R.id.lowestWaterLevel) TextView lowestWaterLevel;
+  @BindView(R.id.currentWaterLevelBottomSpacing) View currentWaterLevelBottomSpacing;
+  @BindView(R.id.currentWaterLevelTopSpacing) View currentWaterLevelTopSpacing;
 
   TideDetailsView(Context context) {
     super(context);
@@ -29,7 +36,34 @@ class TideDetailsView extends BaseScreenView<TideDetailsScreen> {
     bind(this);
   }
 
-  public void setLatestMeasuredTideHeight(BigDecimal latestMeasuredTideHeight) {
+  public void setTideHeights(
+      BigDecimal latestMeasuredTideHeight, BigDecimal lowestMeasuredTideHeight,
+      BigDecimal highestMeasuredTideHeight) {
+    showContent();
+    displayWaterLevelText(latestMeasuredTideHeight, lowestMeasuredTideHeight,
+        highestMeasuredTideHeight);
+
+    float percentOfMax = latestMeasuredTideHeight.subtract(lowestMeasuredTideHeight)
+        .divide(highestMeasuredTideHeight.subtract(lowestMeasuredTideHeight), RoundingMode.HALF_UP)
+        .multiply(BigDecimal.valueOf(100)).setScale(0, BigDecimal.ROUND_HALF_UP).floatValue();
+    currentWaterLevelBottomSpacing.setLayoutParams(new LinearLayout.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT, 2, percentOfMax));
+    currentWaterLevelTopSpacing.setLayoutParams(new LinearLayout.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT, 2, 100 - percentOfMax));
+  }
+
+  private void displayWaterLevelText(
+      BigDecimal latestMeasuredTideHeight, BigDecimal lowestMeasuredTideHeight,
+      BigDecimal highestMeasuredTideHeight) {
+    currentWaterLevel.setText(String.format(Locale.US, "Current Water Level: %.2f ft",
+        latestMeasuredTideHeight.setScale(2, RoundingMode.HALF_UP)));
+    highestWaterLevel.setText(String.format(Locale.US, "Today's Highest Water Level: %.2f ft",
+        highestMeasuredTideHeight.setScale(2, RoundingMode.HALF_UP)));
+    lowestWaterLevel.setText(String.format(Locale.US, "Today's Lowest Water Level: %.2f ft",
+        lowestMeasuredTideHeight.setScale(2, RoundingMode.HALF_UP)));
+  }
+
+  private void showContent() {
     content.setAlpha(0f);
     content.setVisibility(VISIBLE);
     loading.animate().alpha(0f).setListener(new AnimatorListenerAdapter() {
@@ -39,7 +73,6 @@ class TideDetailsView extends BaseScreenView<TideDetailsScreen> {
         loading.setVisibility(GONE);
       }
     }).start();
-    currentWaterLevel.setText(String.format(Locale.US, "%f ft", latestMeasuredTideHeight));
   }
 
 }
