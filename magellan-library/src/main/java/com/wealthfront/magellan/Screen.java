454d0d699106727d166f9523155744b57b5759c6
Jon <jholtan08@gmail.com>
2018-04-02 14:43:52 -0700

    Added ScreenAwareActivity to add flexibility to use other Activities.

--- magellan-library/src/main/java/com/wealthfront/magellan/Screen.java
+++ magellan-library/src/main/java/com/wealthfront/magellan/Screen.java
@@ -1,6 +1,5 @@
 package com.wealthfront.magellan;
 
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
@@ -8,6 +7,7 @@ import android.os.Parcelable;
 import android.support.annotation.ColorRes;
 import android.support.annotation.StringRes;
 import android.support.annotation.VisibleForTesting;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.Menu;
 import android.view.ViewGroup;
@@ -39,100 +39,106 @@ import static com.wealthfront.magellan.Preconditions.checkState;
  */
 public abstract class Screen<V extends ViewGroup & ScreenView> implements BackHandler {
 
-  public static final int DEFAULT_ACTION_BAR_COLOR_RES = 0;
-  private static final String VIEW_STATE = "com.wealthfront.navigation.Screen.viewState";
+    public static final int DEFAULT_ACTION_BAR_COLOR_RES = 0;
+    private static final String VIEW_STATE = "com.wealthfront.navigation.Screen.viewState";
 
-  private Activity activity;
-  private Navigator navigator;
-  private V view;
-  private DialogCreator dialogCreator;
-  private boolean dialogIsShowing;
-  private Dialog dialog;
-  private SparseArray<Parcelable> viewState;
+    private ScreenAwareActivity activity;
+    private Navigator navigator;
+    private V view;
+    private DialogCreator dialogCreator;
+    private boolean dialogIsShowing;
+    private Dialog dialog;
+    private SparseArray<Parcelable> viewState;
   private boolean isTransitioning;
   private Queue<TransitionFinishedListener> transitionFinishedListeners = new LinkedList<>();
 
-  /**
-   * @return the View associated with this Screen or null if we are not in between {@link #onShow(Context)} and\
-   * {@link #onHide(Context)}.
-   */
-  public final V getView() {
-    return view;
-  }
-
-  /**
-   * @return the Activity associated with this Screen or null if we are not in between {@link #onShow(Context)} and\
-   * {@link #onHide(Context)}.
-   */
-  public final Activity getActivity() {
-    return activity;
-  }
-
-  /**
-   * @return the Navigator associated with this Screen.
-   */
-  public final Navigator getNavigator() {
-    return navigator;
-  }
-
-  public final Dialog getDialog() {
-    return dialog;
-  }
-
-  final void restore(Bundle savedInstanceState) {
-    if (viewState == null && savedInstanceState != null) {
-      viewState = savedInstanceState.getSparseParcelableArray(VIEW_STATE + hashCode());
+    /**
+     * @return the View associated with this Screen or null if we are not in between {@link #onShow(Context)} and\
+     * {@link #onHide(Context)}.
+     */
+    public final V getView() {
+        return view;
     }
-  }
 
-  final V recreateView(Activity activity, Navigator navigator) {
-    this.activity = activity;
-    this.navigator = navigator;
-    view = createView(activity);
-    // noinspection unchecked
-    view.setScreen(this);
-    if (viewState != null) {
-      view.restoreHierarchyState(viewState);
+    /**
+     * @return the Activity associated with this Screen or null if we are not in between {@link #onShow(Context)} and\
+     * {@link #onHide(Context)}.
+     */
+    public final ScreenAwareActivity getActivity() {
+        return activity;
     }
-    return view;
-  }
 
-  final void save(Bundle outState) {
-    saveViewState();
-    if (viewState != null) {
-      outState.putSparseParcelableArray(VIEW_STATE + hashCode(), viewState);
+    /**
+     * @return the Navigator associated with this Screen.
+     */
+    public final Navigator getNavigator() {
+        return navigator;
     }
-    viewState = null;
-  }
 
-  final void destroyView() {
-    saveViewState();
-    activity = null;
-    view = null;
-  }
-
-  private void saveViewState() {
-    if (view != null) {
-      viewState = new SparseArray<>();
-      view.saveHierarchyState(viewState);
+    public final Dialog getDialog() {
+        return dialog;
     }
-  }
 
-  final void createDialog() {
-    if (dialogCreator != null && dialogIsShowing) {
-      dialog = dialogCreator.createDialog(activity);
-      dialog.show();
+    final void restore(Bundle savedInstanceState) {
+        if (viewState == null && savedInstanceState != null) {
+            viewState = savedInstanceState.getSparseParcelableArray(VIEW_STATE + hashCode());
+        }
     }
-  }
 
-  final void destroyDialog() {
-    if (dialog != null) {
-      dialogIsShowing = dialog.isShowing();
-      dialog.setOnDismissListener(null);
-      dialog.dismiss();
-      dialog = null;
+    final V recreateView(ScreenAwareActivity activity, Navigator navigator) {
+        this.activity = activity;
+        this.navigator = navigator;
+        if(activity == null){
+            view = createView(null);
+        } else {
+            view = createView(activity.getContext());
+        }
+
+        // noinspection unchecked
+        view.setScreen(this);
+        if (viewState != null) {
+            view.restoreHierarchyState(viewState);
+        }
+        return view;
     }
-  }
+
+    final void save(Bundle outState) {
+        saveViewState();
+        if (viewState != null) {
+            outState.putSparseParcelableArray(VIEW_STATE + hashCode(), viewState);
+        }
+        viewState = null;
+    }
+
+    final void destroyView() {
+        saveViewState();
+        activity = null;
+        view = null;
+    }
+
+    private void saveViewState() {
+        if (view != null) {
+            viewState = new SparseArray<>();
+            view.saveHierarchyState(viewState);
+        }
+    }
+
+    @Deprecated
+    final void createDialog() {
+//    if (dialogCreator != null && dialogIsShowing) {
+//      dialog = dialogCreator.createDialog(carActivity);
+//      dialog.show();
+//    }
+    }
+
+    final void destroyDialog() {
+        if (dialog != null) {
+            dialogIsShowing = dialog.isShowing();
+            dialog.setOnDismissListener(null);
+            dialog.dismiss();
+            dialog = null;
+        }
+    }
 
   void transitionStarted() {
     isTransitioning = true;
@@ -157,134 +163,139 @@ public abstract class Screen<V extends ViewGroup & ScreenView> implements BackHa
     } else {
       listener.onTransitionFinished();
     }
-  }
-
-  /**
+  }/**
    * @return true if we should show the ActionBar, false otherwise (true by default).
    */
   protected boolean shouldShowActionBar() {
     return true;
   }
 
-  /**
-   * @return true if we should animate the ActionBar, false otherwise (true by default).
-   */
-  protected boolean shouldAnimateActionBar() {
-    return true;
-  }
-
-  public String getTitle(Context context) {
-    return "";
-  }
-
-  /**
-   * @return the color of the ActionBar (invalid by default).
-   */
-  @ColorRes
-  protected int getActionBarColorRes() {
-    return DEFAULT_ACTION_BAR_COLOR_RES;
-  }
-
-  protected void onRestore(Bundle savedInstanceState) {}
-
-  /**
-   * The only mandatory method to implement in a Screen. <b>Must</b> create and return a new instance of the View
-   * to be displayed for this Screen.
-   */
-  protected abstract V createView(Context context);
-
-  /**
-   * Override this method to dynamically change the menu.
-   */
-  protected void onUpdateMenu(Menu menu) {}
-
-  /**
-   * Called when the Activity is resumed and when the Screen is shown.
-   */
-  protected void onResume(Context context) {}
-
-  /**
-   * Called when the Screen in shown (including on rotation).
-   */
-  protected void onShow(Context context) {}
-
-  protected void onSave(Bundle outState) {}
-
-  /**
-   * Called when the Activity is paused and when the Screen is hidden.
-   */
-  protected void onPause(Context context) {}
-
-  /**
-   * Called when the Screen is hidden (including on rotation).
-   */
-  protected void onHide(Context context) {}
-
-  /**
-   * Finish the Activity, and therefore quit the app in a Single Activity Architecture.
-   */
-  protected final boolean quit() {
-    if (getActivity() != null) {
-      getActivity().finish();
+    /**
+     * @return true if we should animate the ActionBar, false otherwise (true by default).
+     */
+    protected boolean shouldAnimateActionBar() {
+        return true;
     }
-    return true;
-  }
 
-  protected final void setTitle(@StringRes int titleResId) {
-    activity.setTitle(titleResId);
-  }
+    public String getTitle(Context context) {
+        return "";
+    }
 
-  protected final void setTitle(CharSequence title) {
-    activity.setTitle(title);
-  }
+    /**
+     * @return the color of the ActionBar (invalid by default).
+     */
+    @ColorRes
+    protected int getActionBarColorRes() {
+        return DEFAULT_ACTION_BAR_COLOR_RES;
+    }
 
-  /**
-   * Display a {@link Dialog} using a {@link DialogCreator}. The dialog will be automatically recreated and redisplayed
-   * on rotation.
-   */
-  protected final void showDialog(DialogCreator dialogCreator) {
-    this.dialogCreator = dialogCreator;
-    this.dialogIsShowing = true;
-    createDialog();
-  }
+    protected void onRestore(Bundle savedInstanceState) {
+    }
 
-  /**
-   * Override this method to implement a custom behavior one back pressed.
-   *
-   * @return true if the method consumed the back event, false otherwise.
-   */
-  @Override
-  public boolean handleBack() {
-    return false;
-  }
+    /**
+     * The only mandatory method to implement in a Screen. <b>Must</b> create and return a new instance of the View
+     * to be displayed for this Screen.
+     */
+    protected abstract V createView(Context context);
 
-  /**
-   * @return a String representation of the Screen to be used for logging purposes. Return the Simple name of the class
-   * by default.
-   */
-  @Override
-  public String toString() {
-    return getClass().getSimpleName();
-  }
+    /**
+     * Override this method to dynamically change the menu.
+     */
+    protected void onUpdateMenu(Menu menu) {
+    }
 
-  @VisibleForTesting
-  public final void setView(V view) {
-    this.view = view;
-  }
+    /**
+     * Called when the Activity is resumed and when the Screen is shown.
+     */
+    protected void onResume(Context context) {
+    }
 
-  @VisibleForTesting
-  public final void setActivity(Activity activity) {
-    this.activity = activity;
-  }
+    /**
+     * Called when the Screen in shown (including on rotation).
+     */
+    protected void onShow(Context context) {
+    }
 
-  @VisibleForTesting
-  public final void setNavigator(Navigator navigator) {
-    this.navigator = navigator;
-  }
+    protected void onSave(Bundle outState) {
+    }
 
-  protected final void checkOnCreateNotYetCalled(String reason) {
-    checkState(activity == null, reason);
-  }
+    /**
+     * Called when the Activity is pauseZOxld and when the Screen is hidden.
+     */
+    protected void onPause(Context context) {
+    }
+
+    /**
+     * Called when the Screen is hidden (including on rotation).
+     */
+    protected void onHide(Context context) {
+    }
+
+    /**
+     * Finish the Activity, and therefore quit the app in a Single Activity Architecture.
+     */
+    protected final boolean quit() {
+        if (getActivity() != null) {
+            getActivity().onDestroy();
+        }
+        return true;
+    }
+
+    protected final void setTitle(@StringRes int titleResId) {
+        activity.setTitle(titleResId);
+    }
+
+    protected final void setTitle(CharSequence title) {
+        activity.setTitle(title);
+    }
+
+    /**
+     * Display a {@link Dialog} using a {@link DialogCreator}. The dialog will be automatically recreated and redisplayed
+     * on rotation.
+     */
+    protected final void showDialog(DialogCreator dialogCreator) {
+        this.dialogCreator = dialogCreator;
+        this.dialogIsShowing = true;
+        createDialog();
+    }
+
+    /**
+     * Override this method to implement a custom behavior one back pressed.
+     *
+     * @return true if the method consumed the back event, false otherwise.
+     */
+    @Override
+    public boolean handleBack() {
+        return false;
+    }
+
+    /**
+     * @return a String representation of the Screen to be used for logging purposes. Return the Simple name of the class
+     * by default.
+     */
+    @Override
+    public String toString() {
+        return getClass().getSimpleName();
+    }
+
+    @VisibleForTesting
+    public final void setView(V view) {
+        this.view = view;
+    }
+
+    @VisibleForTesting
+    public final void setActivity(ScreenAwareActivity activity) {
+        this.activity = activity;
+    }
+
+    @VisibleForTesting
+    public final void setNavigator(Navigator navigator) {
+        this.navigator = navigator;
+    }
+
+    protected final void checkOnCreateNotYetCalled(String reason) {
+        checkState(activity == null, reason);
+    }
 
   /**
    * A simple interface with a method to be run when the transition to this screen is finished, or immediately if it's
--- magellan-support/src/main/java/com/wealthfront/magellan/support/SingleActivity.java
+++ magellan-support/src/main/java/com/wealthfront/magellan/support/SingleActivity.java
@@ -1,35 +1,38 @@
 package com.wealthfront.magellan.support;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 
+import com.wealthfront.magellan.ActionBarConfig;
 import com.wealthfront.magellan.Navigator;
+import com.wealthfront.magellan.ScreenAwareActivity;
 
-public abstract class SingleActivity extends AppCompatActivity {
+public abstract class SingleActivity extends AppCompatActivity implements ScreenAwareActivity {
 
-  private static Navigator navigator;
+    private static Navigator navigator;
 
-  protected abstract Navigator createNavigator();
+    protected abstract Navigator createNavigator();
 
-  public static Navigator getNavigator() {
-    return navigator;
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    if (navigator == null) {
-      navigator = createNavigator();
+    public static Navigator getNavigator() {
+        return navigator;
     }
-  }
 
-  @Override
-  protected void onPostCreate(@Nullable Bundle savedInstanceState) {
-    navigator.onCreate(this, savedInstanceState);
-    super.onPostCreate(savedInstanceState);
-  }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (navigator == null) {
+            navigator = createNavigator();
+        }
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        navigator.onCreate(this, savedInstanceState);
+        super.onPostCreate(savedInstanceState);
+    }
 
   @Override
   public void onSaveInstanceState(Bundle outState) {
@@ -37,41 +40,45 @@ public abstract class SingleActivity extends AppCompatActivity {
     super.onSaveInstanceState(outState);
   }
 
-  @Override
-  protected void onResume() {
-    super.onResume();
-    navigator.onResume(this);
-  }
-
-  @Override
-  protected void onPause() {
-    navigator.onPause(this);
-    super.onPause();
-  }
-
-  @Override
-  protected void onDestroy() {
-    navigator.onDestroy(this);
-    super.onDestroy();
-  }
-
-  @Override
-  public void onBackPressed() {
-    if (!navigator.handleBack()) {
-      super.onBackPressed();
+    @Override
+    protected void onResume() {
+        super.onResume();
+        navigator.onResume(this);
     }
-  }
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    navigator.onCreateOptionsMenu(menu);
-    return super.onCreateOptionsMenu(menu);
-  }
+    @Override
+    protected void onPause() {
+        navigator.onPause(this);
+        super.onPause();
+    }
 
-  @Override
-  public boolean onPrepareOptionsMenu(Menu menu) {
-    navigator.onPrepareOptionsMenu(menu);
-    return super.onPrepareOptionsMenu(menu);
-  }
+    @Override
+    public void onDestroy() {
+        navigator.onDestroy(this);
+        super.onDestroy();
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (!navigator.handleBack()) {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        navigator.onCreateOptionsMenu(menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        navigator.onPrepareOptionsMenu(menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public Context getContext() {
+        return this;
+    }
 }
