7784ff133467729e14f86a6dd7f890dbd62b903f
Naren <narendrakumar@wealthfront.com>
2020-07-15 19:30:44 -0700

    Integrate buildSrc for dependency management

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,46 @@
+import Versions.archVersion
+import Versions.butterKnifeVersion
+import Versions.daggerVersion
+import Versions.jacksonVersion
+import Versions.junitVersion
+import Versions.kotlinVersion
+import Versions.lifecycleVersion
+import Versions.mockitoVersion
+import Versions.okhttpVersion
+import Versions.retrofitVersion
+import Versions.robolectricVersion
+import Versions.rxandroidVersion
+import Versions.rxjava2Version
+import Versions.rxjavaAdapterVersion
+import Versions.rxjavaVersion
+import Versions.supportLibVersion
+import Versions.truthVersion
+
+object Dependencies {
+
+  const val kotlinJdk = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
+  const val appCompat = "androidx.appcompat:appcompat:$supportLibVersion"
+  const val lifecycle = "androidx.lifecycle:lifecycle-common-java8:$lifecycleVersion"
+
+  const val junit = "junit:junit:$junitVersion"
+  const val truth = "com.google.truth:truth:$truthVersion"
+  const val mockito = "org.mockito:mockito-core:$mockitoVersion"
+  const val archTesting = "androidx.arch.core:core-testing:$archVersion"
+  const val robolectric = "org.robolectric:robolectric:$robolectricVersion"
+  const val butterknife = "com.jakewharton:butterknife:$butterKnifeVersion"
+  const val butterknifeCompiler = "com.jakewharton:butterknife-compiler:$butterKnifeVersion"
+  const val dagger = "com.google.dagger:dagger:$daggerVersion"
+  const val daggerCompiler = "com.google.dagger:dagger-compiler:$daggerVersion"
+
+  const val retrofit = "com.squareup.retrofit2:retrofit:$retrofitVersion"
+  const val rxjava = "io.reactivex:rxjava:$rxjavaVersion"
+  const val rxjava2 = "io.reactivex.rxjava2:rxjava:$rxjava2Version"
+  const val rxjavaAdapter = "com.squareup.retrofit2:adapter-rxjava:$rxjavaAdapterVersion"
+  const val rxandroid = "io.reactivex:rxandroid:$rxandroidVersion"
+  const val jackson = "com.squareup.retrofit2:converter-jackson:$jacksonVersion"
+  const val okhttp = "com.squareup.okhttp3:logging-interceptor:$okhttpVersion"
+  
+  const val extJunit = "androidx.test.ext:junit:1.1.1"
+  const val espressoCore = "com.android.support.test.espresso:espresso-core:3.0.2"
+}
+
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,26 @@
+object Versions {
+  const val compileSdkVersion = 30
+  const val minSdkVersion = 15
+  const val targetSdkVersion = 30
+
+  const val kotlinVersion = "1.3.72"
+  const val buildToolsVersion = "29.0.2"
+  const val supportLibVersion = "1.1.0"
+  const val robolectricVersion = "4.3.1"
+  const val archVersion = "2.1.0"
+  const val lifecycleVersion = "2.2.0"
+  const val butterKnifeVersion = "10.0.0"
+  const val daggerVersion = "2.24"
+  const val retrofitVersion = "2.9.0"
+  const val rxjavaVersion = "1.3.8"
+  const val rxjava2Version = "2.2.19"
+  const val rxjavaAdapterVersion = "2.3.0"
+  const val rxandroidVersion = "1.2.1"
+  const val jacksonVersion = "2.7.2"
+  const val okhttpVersion = "4.4.0"
+
+
+  const val junitVersion = "4.13"
+  const val truthVersion = "1.0"
+  const val mockitoVersion = "2.23.4"
+}
