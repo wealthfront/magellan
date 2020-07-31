f353977f2bd6d0825bdcfe7a4e10b144c5c344ce
Naren <narendrakumar@wealthfront.com>
2020-07-31 11:50:18 -0700

    Construct/Visualize the tree for backstack events

--- magellan-library/src/main/java/com/wealthfront/magellan/navigation/NavigationTraverser.kt
+++ magellan-library/src/main/java/com/wealthfront/magellan/navigation/NavigationTraverser.kt
@@ -2,33 +2,57 @@ package com.wealthfront.magellan.navigation
 
 import com.wealthfront.magellan.core.Journey
 import com.wealthfront.magellan.core.Navigable
+import com.wealthfront.magellan.lifecycle.LifecycleOwner
 
 class NavigationTraverser(private val root: Journey<*>) {
 
-  fun getGlobalBackStack(): List<Navigable> {
-    val backStackList = mutableListOf<Navigable>()
-    exploreBackStack(root, backStackList)
-    return backStackList.toList()
+  fun getGlobalBackStack(): NavigationNode {
+    return constructTree(root)
   }
 
-  fun getVisibleNavigables(): List<Navigable> {
-    val globalBackStack = getGlobalBackStack()
-    return globalBackStack.filter { it.view != null }
-  }
-
-  private fun exploreBackStack(journey: Journey<*>, backStackList: MutableList<Navigable>) {
-    val backStack = getBackStackForJourney(journey)
-    if (backStack.isEmpty()) return
-    backStack.forEach { navEvent ->
-      val nav = navEvent.navigable
-      backStackList.add(nav)
-      if (nav is Journey<*>) {
-        exploreBackStack(nav, backStackList)
+  private fun constructTree(navigable: Navigable): NavigationNode {
+    val navNode = NavigationNode(navigable, mutableListOf())
+    if (navNode.hasBackstack) {
+      navNode.backStack.forEach { navEvent ->
+        val childNavNode = constructTree(navEvent.navigable)
+        navNode.addChild(childNavNode)
       }
     }
+    return navNode
   }
 
-  private fun getBackStackForJourney(journey: Journey<*>): List<NavigationEvent> {
-    return journey.children.mapNotNull { it as? Navigator }.first().backStack
+  fun printGlobalBackstack(): String {
+    val rootNode = getGlobalBackStack()
+    val sb = StringBuilder()
+    getPrintableGlobalBackstack(rootNode, 0, sb)
+    return sb.toString()
+  }
+
+  private fun getPrintableGlobalBackstack(navNode: NavigationNode, depth: Int, sb: StringBuilder) {
+    sb.appendln()
+    for (i in 0 until depth) {
+      sb.append("\t")
+    }
+    sb.append(navNode.value.javaClass.simpleName)
+    navNode.children.forEach { childNode ->
+      getPrintableGlobalBackstack(childNode, depth + 1, sb)
+    }
+  }
+}
+
+data class NavigationNode(
+  val value: Navigable,
+  var children: List<NavigationNode>
+) {
+
+  val hasBackstack: Boolean
+    get() = (value as? LifecycleOwner)?.children?.mapNotNull { it as? Navigator }?.isNotEmpty()
+      ?: false
+
+  val backStack: List<NavigationEvent>
+    get() = (value as LifecycleOwner).children.mapNotNull { it as? Navigator }.first().backStack
+
+  fun addChild(child: NavigationNode) {
+    children = children + child
   }
 }
