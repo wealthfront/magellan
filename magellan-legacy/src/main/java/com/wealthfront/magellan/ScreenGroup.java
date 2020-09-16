745682749548a2995fe7a6c90f0808201f950187
Naren <narendrakumar@wealthfront.com>
2020-09-16 14:27:07 -0700

    More nullability

--- magellan-legacy/src/main/java/com/wealthfront/magellan/ScreenGroup.java
+++ magellan-legacy/src/main/java/com/wealthfront/magellan/ScreenGroup.java
@@ -1,41 +1,52 @@
 package com.wealthfront.magellan;
 
+import android.content.Context;
 import android.view.ViewGroup;
 
-import com.wealthfront.magellan.lifecycle.LifecycleState;
+import com.wealthfront.magellan.lifecycle.LifecycleAware;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static com.wealthfront.magellan.lifecycle.LifecycleState.*;
 
 /**
  * A Screen containing a list of screens. Useful to display reusable Screens that can be either in another one or on
  * it's own.
  */
-public abstract class ScreenGroup<S extends Screen, V extends ViewGroup & ScreenView> extends Screen<V> {
+public abstract class ScreenGroup<S extends Screen, V extends ViewGroup & ScreenView> extends Screen<V> implements MultiScreen<S> {
 
   private List<S> screens;
 
   public ScreenGroup() {
-    this.screens = new ArrayList<>();
+    this(new ArrayList<>());
   }
 
-  public ScreenGroup(List<S> screens) {
+  public ScreenGroup(@NotNull List<S> screens) {
     this.screens = new ArrayList<>(screens);
+    addScreens(screens);
   }
 
-  public void addScreen(S screen) {
-    attachToLifecycle(screen, LifecycleState.Destroyed.INSTANCE);
+  @Override
+  public void addScreen(@NotNull S screen) {
     screens.add(screen);
+    attachToLifecycleWithNavigator(screen);
   }
 
-  public void addScreens(List<S> screens) {
-    for (S screen : screens) {
-      addScreen(screen);
-    }
-  }
-
-  protected final List<S> getScreens() {
+  @NotNull
+  public final List<S> getScreens() {
     return new ArrayList<>(screens);
   }
 
+  private void attachToLifecycleWithNavigator(@NotNull S screen) {
+    attachToLifecycle(screen, Destroyed.INSTANCE);
+    attachToLifecycle(new LifecycleAware() {
+      @Override
+      public void create(@NotNull Context context) {
+        screen.setNavigator(getNavigator());
+      }
+    }, Destroyed.INSTANCE);
+  }
 }
