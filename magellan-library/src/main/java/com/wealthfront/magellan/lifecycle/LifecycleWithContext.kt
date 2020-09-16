41d068d78f65d1c478e1de007d5419f5ffc90a49
Naren <narendrakumar@wealthfront.com>
2020-09-16 12:23:27 -0700

    address comments

---
+++ magellan-library/src/main/java/com/wealthfront/magellan/lifecycle/LifecycleWithContext.kt
@@ -0,0 +1,22 @@
+package com.wealthfront.magellan.lifecycle
+
+import android.content.Context
+
+class LifecycleWithContext<V>(
+  val supplier: (Context) -> V
+) : LifecycleAware {
+
+  var data: V? = null
+    protected set
+
+  override fun show(context: Context) {
+    data = supplier(context)
+  }
+
+  override fun hide(context: Context) {
+    data = null
+  }
+}
+
+fun <V> LifecycleOwner.lifecycleWithContext(supplier: (Context) -> V) =
+  lifecycle(LifecycleWithContext(supplier), { it.data })
