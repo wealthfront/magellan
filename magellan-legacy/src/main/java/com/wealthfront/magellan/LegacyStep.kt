35c5a01a291e35a0d260e504811442209300cf11
cmathew <chrismathew@wealthfront.com>
2021-11-01 11:26:28 -0700

    add legacystep class, requiring a navigator as a constructor to define backstack context

---
+++ magellan-legacy/src/main/java/com/wealthfront/magellan/LegacyStep.kt
@@ -0,0 +1,21 @@
+package com.wealthfront.magellan
+
+import android.content.Context
+import android.view.View
+import androidx.annotation.VisibleForTesting
+import com.wealthfront.magellan.core.Navigable
+import com.wealthfront.magellan.lifecycle.LifecycleAwareComponent
+import com.wealthfront.magellan.lifecycle.attachFieldToLifecycle
+import com.wealthfront.magellan.lifecycle.createAndAttachFieldToLifecycleWhenShown
+import com.wealthfront.magellan.view.DialogComponent
+
+public abstract class LegacyStep<V: View>(protected val navigator: Navigator): LifecycleAwareComponent(), Navigable {
+  protected val dialogComponent: DialogComponent by attachFieldToLifecycle(DialogComponent())
+  
+  override var view: V? by createAndAttachFieldToLifecycleWhenShown { context ->
+    createView(context) 
+  }
+    @VisibleForTesting set
+
+  protected abstract fun createView(context: Context): V
+}
