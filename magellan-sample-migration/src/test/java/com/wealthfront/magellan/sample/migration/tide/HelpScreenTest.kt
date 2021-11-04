b84b07a765877b87de723cb74b70f132f142896c
cmathew <chrismathew@wealthfront.com>
2021-11-03 18:33:25 -0700

    oops fix import ordering

--- magellan-sample-migration/src/test/java/com/wealthfront/magellan/sample/migration/tide/HelpScreenTest.kt
+++ magellan-sample-migration/src/test/java/com/wealthfront/magellan/sample/migration/tide/HelpScreenTest.kt
@@ -14,8 +14,8 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
-import org.mockito.Mockito.`when`
 import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
 import org.mockito.MockitoAnnotations.initMocks
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.Shadows.shadowOf
