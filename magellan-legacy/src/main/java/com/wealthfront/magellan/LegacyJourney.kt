5f57ca0b9e7d581076fd95af1887a0abda9a2608
cmathew <chris.w.mathew@gmail.com>
2022-05-06 13:50:56 -0700

    introduce legacyjourney

---
+++ magellan-legacy/src/main/java/com/wealthfront/magellan/LegacyJourney.kt
@@ -0,0 +1,22 @@
+package com.wealthfront.magellan
+
+import android.view.LayoutInflater
+import androidx.viewbinding.ViewBinding
+import com.wealthfront.magellan.core.Step
+import com.wealthfront.magellan.init.Magellan
+import com.wealthfront.magellan.lifecycle.attachFieldToLifecycle
+import com.wealthfront.magellan.navigation.CurrentNavigableProvider
+import com.wealthfront.magellan.navigation.NavigationOverrideProvider
+import com.wealthfront.magellan.navigation.ViewTemplateApplier
+
+public abstract class LegacyJourney<V : ViewBinding>(
+  createBinding: (LayoutInflater) -> V,
+  container: V.() -> ScreenContainer,
+  navigationOverrides: NavigationOverrideProvider? = Magellan.getNavigationOverrideProvider(),
+  templateApplier: ViewTemplateApplier? = null
+) : Step<V>(createBinding) {
+
+  public open var navigator: Navigator by attachFieldToLifecycle(
+    Navigator({ viewBinding!!.container() }, navigationOverrides, templateApplier)
+  )
+}
